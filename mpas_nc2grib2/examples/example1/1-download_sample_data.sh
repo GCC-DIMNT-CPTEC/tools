27e48c2f1e0cd4affe3d26850b6b283585b38a60
sergioh <sergioh.pessoal@gmail.com>
2024-10-28 20:19:51 -0300

    Using new datata sample

--- mpas_nc2grib2/examples/example1/1-download_sample_data.sh
+++ mpas_nc2grib2/examples/example1/1-download_sample_data.sh
@@ -14,10 +14,10 @@ cd $RUNDIR
 
 mkdir -p ./datain
 cd ./datain
-wget -nc ftp1.cptec.inpe.br/avalia/avalia/samples/monan/nc/MONAN_DIAG_G_POS_GFS_2024070900_2024070900.x1024002L55.nc
-wget -nc ftp1.cptec.inpe.br/avalia/avalia/samples/monan/nc/MONAN_DIAG_G_POS_GFS_2024070900_2024070903.x1024002L55.nc
-wget -nc ftp1.cptec.inpe.br/avalia/avalia/samples/monan/nc/MONAN_DIAG_G_POS_GFS_2024070900_2024070906.x1024002L55.nc
-wget -nc ftp1.cptec.inpe.br/avalia/avalia/samples/monan/nc/MONAN_DIAG_G_POS_GFS_2024070900_2024070912.x1024002L55.nc
-wget -nc ftp1.cptec.inpe.br/avalia/avalia/samples/monan/nc/MONAN_DIAG_G_POS_GFS_2024070900_2024071000.x1024002L55.nc
+wget -nc ftp1.cptec.inpe.br/avalia/avalia/samples/monan/nc/MONAN_DIAG_G_POS_GFS_2024100100_2024100100.x1024002L55.nc
+wget -nc ftp1.cptec.inpe.br/avalia/avalia/samples/monan/nc/MONAN_DIAG_G_POS_GFS_2024100100_2024100106.x1024002L55.nc
+wget -nc ftp1.cptec.inpe.br/avalia/avalia/samples/monan/nc/MONAN_DIAG_G_POS_GFS_2024100100_2024100112.x1024002L55.nc
+wget -nc ftp1.cptec.inpe.br/avalia/avalia/samples/monan/nc/MONAN_DIAG_G_POS_GFS_2024100100_2024100118.x1024002L55.nc
+wget -nc ftp1.cptec.inpe.br/avalia/avalia/samples/monan/nc/MONAN_DIAG_G_POS_GFS_2024100100_2024100200.x1024002L55.nc
 
 
