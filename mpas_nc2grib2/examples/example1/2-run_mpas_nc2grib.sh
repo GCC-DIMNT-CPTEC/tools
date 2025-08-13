db18737753d66ee4418bb36b08218aaccaf76889
sergio.ferreira@inpe.br <sergioh.pessoal@gmail.com>
2025-08-13 13:50:17 -0300

    small changes

--- mpas_nc2grib2/examples/example1/2-run_mpas_nc2grib.sh
+++ mpas_nc2grib2/examples/example1/2-run_mpas_nc2grib.sh
@@ -5,9 +5,10 @@ if [[ $a == *"egeon"* ]]; then
 	module load cdo-2.0.4-gcc-9.4.0-bjulvnd
 	module load netcdf-fortran
 	export NFDIR=/opt/ohpc/pub/libs/gnu9/openmpi4/netcdf-fortran/4.5.3
-else
-	export NC2GRIB_DIR=../../
 fi
+
+export NC2GRIB_DIR=../../
+
 
 dirin=./datain
 dirout=./dataout
@@ -22,6 +23,5 @@ for fff in 000 003 006 012 024 ; do
     forecast_time=$yy2$mm2$dd2$hh2
 
     filein=$dirin/MONAN_DIAG_G_POS_GFS_${start_time}_${forecast_time}.x1024002L55.nc
-
     $NC2GRIB_DIR/bin/mpas_nc2grib2.x -i $filein -o $fileout -s $start_time -f $fff -v 2
 done
