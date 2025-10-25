63e0b4c6e6874602684b5a1736329c7f47c18152
sergioh <sergioh.pessoal@gmail.com>
2025-10-25 09:33:22 -0300

    siple addition

---
+++ mpas_nc2grib2/examples/example4/3-run_g2ctl.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+#----------------------------------------------------------------------------#
+#  run_g2ctl: A CTL and iDX files are produced from grib2 to be used in      #
+#             grads software                                                 #
+#----------------------------------------------------------------------------#
+echo "HOST="$a
+if [[ $a == *"egeon"* ]]; then
+	module load cdo-2.0.4-gcc-9.4.0-bjulvnd
+	module load netcdf-fortran
+	export NFDIR=/opt/ohpc/pub/libs/gnu9/openmpi4/netcdf-fortran/4.5.3
+else
+	export NC2GRIB_DIR=../..
+fi
+
+monandir="tc/1.4.1-rc/P1"
+sufix='x1024002L18'
+
+dirout=./dataout
+
+start_time=2024050100
+for fff in 000 ; do 
+   source ./get_date.sh ${start_time}${fff}
+   forecast_time=$yy2$mm2$dd2$hh2
+
+   dirout=./dataout/GRIB2/$yy0$mm0$dd0$hh0
+   echo "Origin="$dirin
+   echo "Destino="$dirout
+   cd $dirout
+   gribfile='./MONAN_DIAG_G_POS_GFS_'${start_time}_${forecast_time}'.'$sufix'.grib2'
+
+   if [ -f "$gribfile" ]; then
+      ls -ltr $gribfile
+      g2ctl -verf $gribfile >  $gribfile.ctl
+      gribmap -i $gribfile.ctl
+   else
+      echo "ERROR!: File not found"
+      echo $gribfile
+   fi
+
+
+done
